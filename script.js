d7a41e181db4071c56cb73a225a41518b92f4203
IvanBrescovich <ivanbrescovcih@gmail.com>
2022-07-06 20:41:29 -0300

    Primer Commit

---
+++ script.js
@@ -0,0 +1,82 @@
+//Exercises Module 1
+
+/*1) Pedir al usuario que ingrese su nombre. Guardar
+ese nombre en una variable y utilizarlo para saludar al
+usuario.
+*/
+
+//const firstName = prompt("Ingresa tu nombre");
+
+//alert(`Hola ${firstName} mucho gusto
+//`);
+
+/*2) Pedirle a un usuario que ingrese dos numeros
+y devolverle la suma de dichos numeros
+*/
+
+//const numOne = prompt("Ingrese un numero");
+//const numTwo = prompt("Ingrese otro numero");
+
+//alert(`La suma de  ${numOne} y ${numTwo} es  ${+numOne + +numTwo}
+//`);
+
+/*3) Pedirle a un usuario que ingrese dos numeros
+y devolverle el doble de la suma de dichos numeros
+*/
+
+//const numOne = prompt("Ingrese un numero");
+//const numTwo = prompt("Ingrese otro numero");
+
+//alert(
+//  `El doble de la suma de  ${numOne} y ${numTwo} es  ${(+numOne + +numTwo) * 2}
+//`);
+
+/*
+4) Pedirle al usuario que ingrese el ancho y el alto 
+de una habitacion y calcular la superficie.
+*/
+
+//const height = prompt("Ingrese la altura de la habitación");
+//const width = prompt("Ingrese el ancho de la habitación");
+//const result = height * width;
+
+//alert(`La superficie de la habitación es ${result} metros cuadrados
+//`);
+
+/*
+5) Pedirle al usuario que ingrese su nombre y devolverle
+el nombre todo en minusculas
+*/
+
+//const firstName = prompt("Ingrese su nombre");
+
+//alert(`Tu nombre en minusculas es ${firstName.toLowerCase()}
+//`);
+
+/*
+6) Pedirle al usuario que ingrese su apellido y devolverle
+el apellido todo en mayuscula.
+*/
+
+//const lastName = prompt("Ingrese su apellido");
+
+//alert(`Tu apellido en mayusculas es ${lastName.toLocaleUpperCase()}
+//`);
+
+/*
+7) Pedirle al usuario el nombre, el apellido y la 
+edad. Mostrar el siguiente mensaje:
+Nombre: Juan
+Apellido: Perez
+Edad: 20
+*/
+
+//const firstName = prompt("Ingrese su nombre");
+//const lastName = prompt("Ingrese su apellido");
+//const age = prompt("Ingrese su edad");
+
+//alert(`Tus datos son:
+//Nombre: ${firstName}
+//Apellido: ${lastName}
+//Edad: ${age}
+//`);
